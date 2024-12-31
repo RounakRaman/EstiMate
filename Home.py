52d2f5438a402623480221e8f4afd6cd2b1b011e
Rounak Raman <rounak.raman.ug21@nsut.ac.in>
2024-12-31 14:09:23 +0530

    First MVP for EstiMate

--- Home.py
+++ Home.py
@@ -8,7 +8,7 @@ from typing import List, Dict
 import random
 import time
 import os
-from pyngrok import ngrok
+
 
 
 #######################################################################################
