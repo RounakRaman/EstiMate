38861acbc2bc75233405802a45eac413ce254f70
Rounak Raman <rounak.raman.ug21@nsut.ac.in>
2025-01-01 17:44:50 +0530

    Added database for storing user Data

--- Home.py
+++ Home.py
@@ -424,7 +424,7 @@ def main():
                         },index=[i])
 
                         updated_df=pd.concat([existing_data,feedback_data],ignore_index=True)
-                        st.write(updated_df)
+                        
                         conn.update(worksheet="User Feedback Data",data=updated_df)
 
                         st.success("Thank you for your feedback! Your responses have been recorded. You can now download your interview transcript and view your results.")
